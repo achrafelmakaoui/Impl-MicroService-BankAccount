d6c76c38aceae6d90c7f45c16c4899873473aa08
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-09-26 18:28:35 +0100

    Add Rest Full Api

--- src/main/java/com/elmakaoui/bankaccountservice/entities/BankAccount.java
+++ src/main/java/com/elmakaoui/bankaccountservice/entities/BankAccount.java
@@ -17,7 +17,7 @@ public class BankAccount {
     @Id
     private String id;
     private Date createAt;
-    private double balance;
+    private Double balance;
     private String currency;
     @Enumerated(EnumType.STRING)
     private AccountType type;
--- src/main/java/com/elmakaoui/bankaccountservice/repositories/BankAccountRepository.java
+++ src/main/java/com/elmakaoui/bankaccountservice/repositories/BankAccountRepository.java
@@ -2,6 +2,8 @@ package com.elmakaoui.bankaccountservice.repositories;
 
 import com.elmakaoui.bankaccountservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
 }
